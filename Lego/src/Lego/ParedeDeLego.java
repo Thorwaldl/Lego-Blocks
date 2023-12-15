aeb9131df5d65a1e7665bc2a527d21d097cc81b1
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2023-12-14 21:17:20 -0300

    Update ParedeDeLego.java

--- Lego/src/Lego/ParedeDeLego.java
+++ Lego/src/Lego/ParedeDeLego.java
@@ -1,28 +1,29 @@
-package Lego;
+import java.util.Scanner;
 
-public class ParedeDeLego {
-
+public class Main {
     private static final int MOD = 1000000007;
-    private int altura;
-    private int largura;
 
-    public ParedeDeLego(int altura, int largura) {
-        this.altura = altura;
-        this.largura = largura;
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public int calculaManeirasDeConstruir() {
-        int[] dp = new int[largura + 1];
-        dp[0] = 1;
+        // Solicitar ao usuário o número de casos de teste
+        System.out.print("Digite o número de casos de teste (t): ");
+        int t = scanner.nextInt();
 
-        for (int i = 1; i <= largura; i++) {
-            for (int j = 1; j <= 4; j++) {
-                if (i >= j) {
-                    dp[i] = (dp[i] + dp[i - j]) % MOD;
-                }
-            }
+        for (int i = 0; i < t; i++) {
+            // Leitura de n e m para cada caso de teste
+            System.out.print("Digite a altura (n) e a largura (m) separadas por espaço para o caso de teste " + (i + 1) + ": ");
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+
+            // Criando um objeto ParedeDeLego
+            ParedeDeLego parede = new ParedeDeLego(n, m);
+
+            // Chamada do método calculaManeirasDeConstruir e impressão do resultado
+            int result = parede.calculaManeirasDeConstruir();
+            System.out.println("Número de maneiras de construir a parede: " + result);
         }
 
-        return dp[largura];
+        scanner.close();
     }
 }
