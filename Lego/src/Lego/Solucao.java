ae6c641c8fed6d03123358724cb73fed1815ee0c
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2023-12-14 21:17:40 -0300

    Update Solucao.java

--- Lego/src/Lego/Solucao.java
+++ Lego/src/Lego/Solucao.java
@@ -33,43 +33,110 @@ public class Solucao {
     	
 
         // Exibindo as linhas de soma encontradas
+    	
         System.out.println("Formas de soma para " + largura + ":");
         for (List<Integer> forma : formasLinha) {
             System.out.println(forma);
         }
 
-
-        // Mostrando as permutações geradas
+        
+    	//para cada permutação de linhas é gerada uma matriz
         for (List<Integer> permutacao : combinacoesLinhas) {
-            System.out.println(permutacao);
+        	montaMatriz(permutacao);
+            //System.out.println(permutacao);
+
         }
+        
+        System.out.println("TOTAL DE FORMAS"+combinacoesLinhas.size());
+        System.out.println("TOTAL DE FORMAS VÁLIDAS:"+retorno);
+
     	
     }
     
 
     
-    public void montaLinha() {
-    	int soma=0;
+    public void montaMatriz(List<Integer> permutacao) {
+    	int alt=0;
     	
-    	if(soma<largura) {
+    	for(int permut=0;permut<permutacao.size() ;permut++) {
+
+            	System.out.print(formasLinha.get(permutacao.get(permut)-1));
+            	preencheMatriz(formasLinha.get(permutacao.get(permut)-1),alt);
+            	alt++;
+    	}
+    	
+        System.out.println("");
+
+    	imprimirMatriz(this.matriz);
+    	if(verificaValidade(this.matriz)) {
+            System.out.println("MATRIZ VÁLIDA");
+    	}else {
+            System.out.println("MATRIZ INVÁLIDA");
+    	}
+    	limparMatriz(this.matriz);
+        System.out.println("");
+        System.out.println("");
+
+    }
+    
+    public void preencheMatriz(List<Integer> linha, int alt) {
+    	int bloco = 0;
+    	int lego=0;
+    	int index=0;
+        System.out.println("quantia de blocos na linha: "+linha.size());
+
+    	for(int lin=0; lin<linha.size();lin++) {
+    		lego=linha.get(lin);
     		
-    		if (soma==largura) {
+    		for (int i=0; i<lego;i++) {
     			
+    	    System.out.println ("this.matriz[" + alt+"]["+index+"]="+lego);
+    		this.matriz[alt][index]=bloco;
+    		index++;
+
+    		}
+    		bloco++;
+    	}
+    	
+
+    }
+    
+
+    
+    public boolean verificaValidade(int[][] matriz) {
+    	int alt=0;
+    	int larg=0;
+    	//matriz[alt][larg]=0;
+    	for(alt=0;alt<this.altura;) {
+    		
+    		if(matriz[alt][larg]==matriz[alt][larg+1]) {
+                System.out.println("Ponto na largura: "+larg);
+    			larg++;
+    			alt=0;
+    			if(larg+1==this.largura) {
+    		    	this.retorno ++;
+    		    	return true;
+    			}
+    		}else {
+    			alt++;
     		}
     	}
-    }
-    
-    public boolean verificaValidade() {
-    	int alt;
-    	boolean valido=true;
-    	for(alt=0;alt<altura-1;alt++) {
-    		
-    		
-    	}
-    	this.retorno ++;
+    	
+    	
     	return false;
     }
     
+    public void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+        System.out.println();
+    }
     
     public static void limparMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
